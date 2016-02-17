e0dce7bb169ea4b690a84d453cde1c193ed13e95
Francesco <fpfrancescopalma7@gmail.com>
2016-02-17 10:01:23 +0000

    Updates gentleman.rb

--- week_1/d2/gentleman.rb
+++ week_1/d2/gentleman.rb
@@ -15,9 +15,9 @@ robot("Gentleman", "Arduino", "Ruby", "Artoo", "To say Hello to people who greet
 puts 'Would you like to have a nice chat with Gentleman?'
 
 user_answer = gets.chomp
-if (user_answer == 'yes') then 
+if (user_answer == 'yes')  
   puts 'All Right! What\'s your name?'
-elsif (user_answer == 'no') then
+elsif (user_answer == 'no')
   puts 'No Worries, have a nice day!'
   abort('End of the program :)')
 else 
@@ -29,7 +29,7 @@ puts "Oh, Hello #{user_name}. What a Wonderful name! What would you like to conv
 puts ">>>> A tip for you: this Robot is still in Beta, please be patient. Your answer should be \'politics\', \'food\' or \'just say hello?\'"
 
 user_topic = gets.chomp
-  if (user_topic == 'politics') then
+  if (user_topic == 'politics')
     puts "I am sorry, I fear I am not an expert on this matter and I couldn\'t add any knowledge nor value to our conversation."
   elsif (user_topic == "food")
     puts "I don\'t really need to eat, therefore I have very little knowledge of food in general. Sorry for the inconvenience." 
