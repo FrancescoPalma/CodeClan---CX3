f4b26ab1040ca994847b8120879ca689662ae762
Francesco <fpfrancescopalma7@gmail.com>
2016-05-19 13:53:08 +0100

    Adds class work day4

---
+++ week_14/d4/rails_comments_api/app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ApplicationController
+  def index
+    comments = Comment.all
+    render( { :json => comments } )
+  end
+
+  def create
+    Comment.create(comment_params)
+    render( { :json => comment } )
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text, :author)
+  end
+end
